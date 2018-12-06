8922298bc72f3bdf967f843932ceb7e984a21eed
Jaidon Rymer <jaidon.rymer@gmail.com>
2018-12-06 12:09:40 +0000

    adding content and three.js

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,22 @@
-$('body').on('click', '.hamburger-toggle', function(e){
+$('body').on('click', '.hamburger-toggle', function (e) {
     e.preventDefault();
     e.stopPropagation();
-    if($('.hamburger-menu').is(":checked"))
-    {
+    if ($('.hamburger-menu').is(":checked")) {
         hideMenuIfVisible();
-    }else{
+    } else {
         $('.hamburger-menu').prop('checked', true);
         $('html').on('click', hideMenuIfVisible);
         $('nav').on('click', stopPropagation);
     }
 })
 
-$('body').on('change', '.category', function(){
-    if(this.checked)
+$('body').on('change', '.category', function () {
+    if (this.checked)
         $(this).parent().siblings().find('.category').prop('checked', false);
 });
 
 function hideMenuIfVisible() {
-    if($('.hamburger-menu').is(":checked"))
-    {
+    if ($('.hamburger-menu').is(":checked")) {
         $('.hamburger-menu, nav input[type="checkbox"]').prop('checked', false);
         $('html').off('click', hideMenuIfVisible);
         $('nav').off('click', stopPropagation);
@@ -26,4 +24,72 @@ function hideMenuIfVisible() {
 }
 function stopPropagation(e) {
     e.stopPropagation();
+}
+
+
+
+
+
+// All of these variables will be needed later, just ignore them for now.
+var container;
+var camera, controls, scene, renderer;
+var lighting, ambient, keyLight, fillLight, backLight;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+init();
+animate();
+
+function init() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 3;
+    scene = new THREE.Scene();
+    ambient = new THREE.AmbientLight(0xffffff, 1.0);
+    scene.add(ambient);
+
+    keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+    keyLight.position.set(-100, 0, 100);
+
+    fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+    fillLight.position.set(100, 0, 100);
+
+    backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+    backLight.position.set(100, 0, -100).normalize();
+
+    scene.add(keyLight);
+    scene.add(fillLight);
+    scene.add(backLight);
+
+    var mtlLoader = new THREE.MTLLoader();
+    mtlLoader.load('Content/walt/WaltHead.mtl', function (materials) {
+
+        materials.preload();
+
+        materials.materials.default.map.magFilter = THREE.NearestFilter;
+        materials.materials.default.map.minFilter = THREE.LinearFilter;
+
+        var objLoader = new THREE.OBJLoader();
+        objLoader.setMaterials(materials);
+        objLoader.load('Content/walt/WaltHead.obj', function (object) {
+
+            scene.add(object);
+
+        });
+
+    });
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(new THREE.Color("hsl(0, 0%, 10%)"));
+
+    container.appendChild(renderer.domElement);
+}
+
+function render() {
+    requestAnimationFrame(render);
+    controls.update();
+    renderer.render(scene, camera);
 }
