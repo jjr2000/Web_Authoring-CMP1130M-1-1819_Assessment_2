6d9481a29bf32edc82dac5ae37512343b2fef32e
Jaidon Rymer <jaidon.rymer@gmail.com>
2018-12-17 18:53:53 +0000

    Some Tweaks

--- JS/main.js
+++ JS/main.js
@@ -39,61 +39,61 @@ init();
 animate();
 function init() {
     container = $(".three-container")[0]
-    document.body.appendChild( container );
-    camera = new THREE.PerspectiveCamera( 45, (window.innerWidth / 2) / (window.innerHeight / 2), 1, 2000 );
+    document.body.appendChild(container);
+    camera = new THREE.PerspectiveCamera(45, (window.innerWidth / 2) / (window.innerHeight / 2), 1, 2000);
     camera.position.z = 250;
     // scene
     scene = new THREE.Scene();
-    var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
-    scene.add( ambientLight );
-    var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-    camera.add( pointLight );
-    scene.add( camera );
+    var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+    scene.add(ambientLight);
+    var pointLight = new THREE.PointLight(0xffffff, 0.8);
+    camera.add(pointLight);
+    scene.add(camera);
     // model
-    var onProgress = function ( xhr ) {
-        if ( xhr.lengthComputable ) {
+    var onProgress = function (xhr) {
+        if (xhr.lengthComputable) {
             var percentComplete = xhr.loaded / xhr.total * 100;
-            console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
+            console.log(Math.round(percentComplete, 2) + '% downloaded');
         }
     };
     var onError = function () { };
     new THREE.MTLLoader()
-        .setPath( 'Content/assets/' )
-        .load( 'vans.mtl', function ( materials ) {
+        .setPath('Content/assets/')
+        .load('vans.mtl', function (materials) {
             materials.preload();
             new THREE.OBJLoader()
-                .setMaterials( materials )
-                .setPath( 'Content/assets/' )
-                .load( 'vans.obj', function ( object ) {
+                .setMaterials(materials)
+                .setPath('Content/assets/')
+                .load('vans.obj', function (object) {
                     object.position.y = - 95;
-                    scene.add( object );
-                }, onProgress, onError );
-        } );
+                    scene.add(object);
+                }, onProgress, onError);
+        });
     //
     renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+    container.appendChild(renderer.domElement);
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
     controls.dampingFactor = 0.25;
     controls.enableZoom = false;
     //
-    window.addEventListener( 'resize', onWindowResize, false );
+    window.addEventListener('resize', onWindowResize, false);
 }
 function onWindowResize() {
     windowHalfX = window.innerWidth / 4;
     windowHalfY = window.innerHeight / 4;
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = (window.innerWidth / 2) / (window.innerHeight / 2);
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
 }
 
 function animate() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     render();
 }
 function render() {
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
