7c8305d89e6bb20994a8ed547568d5ba5f07e207
Jaidon Rymer <jaidon.rymer@gmail.com>
2018-12-17 18:48:25 +0000

    Moved to orbit controls

--- JS/main.js
+++ JS/main.js
@@ -33,15 +33,14 @@ function stopPropagation(e) {
 
 var container, stats;
 var camera, scene, renderer;
-var mouseX = 0, mouseY = 0;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
+var windowHalfX = window.innerWidth / 4;
+var windowHalfY = window.innerHeight / 4;
 init();
 animate();
 function init() {
     container = $(".three-container")[0]
     document.body.appendChild( container );
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera = new THREE.PerspectiveCamera( 45, (window.innerWidth / 2) / (window.innerHeight / 2), 1, 2000 );
     camera.position.z = 250;
     // scene
     scene = new THREE.Scene();
@@ -75,29 +74,26 @@ function init() {
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.enableZoom = false;
     //
     window.addEventListener( 'resize', onWindowResize, false );
 }
 function onWindowResize() {
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
+    windowHalfX = window.innerWidth / 4;
+    windowHalfY = window.innerHeight / 4;
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
-function onDocumentMouseMove( event ) {
-    mouseX = ( event.clientX - windowHalfX ) / 2;
-    mouseY = ( event.clientY - windowHalfY ) / 2;
-}
-//
+
 function animate() {
     requestAnimationFrame( animate );
     render();
 }
 function render() {
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-    camera.position.y += ( - mouseY - camera.position.y ) * .05;
-    camera.lookAt( scene.position );
     renderer.render( scene, camera );
 }
