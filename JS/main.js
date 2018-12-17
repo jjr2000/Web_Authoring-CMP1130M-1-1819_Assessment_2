233ff2e86c3553e9ac9ec2d4535e6a313be62ea3
Jaidon Rymer <jaidon.rymer@gmail.com>
2018-12-17 17:04:12 +0000

    New three js

--- JS/main.js
+++ JS/main.js
@@ -26,83 +26,78 @@ function stopPropagation(e) {
     e.stopPropagation();
 }
 
-if (!Detector.webgl) {
-    Detector.addGetWebGLMessage();
-}
-var container = $(".three-container")[0];
-var camera, controls, scene, renderer;
-var lighting, ambient, keyLight, fillLight, backLight;
+
+
+
+
+
+var container, stats;
+var camera, scene, renderer;
+var mouseX = 0, mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
 init();
 animate();
 function init() {
-    /* Camera */
-    camera = new THREE.PerspectiveCamera(45, (window.innerWidth / 2) / (window.innerHeight / 2), 1, 1000);
-    camera.position.z = 3;
-    /* Scene */
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.z = 250;
+    // scene
     scene = new THREE.Scene();
-    lighting = false;
-    ambient = new THREE.AmbientLight(0xffffff, 1.0);
-    scene.add(ambient);
-    keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-    keyLight.position.set(-100, 0, 100);
-    fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-    fillLight.position.set(100, 0, 100);
-    backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-    backLight.position.set(100, 0, -100).normalize();
-    /* Model */
-    var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setBaseUrl('Content/assets/');
-    mtlLoader.setPath('Content/assets/');
-    mtlLoader.load('ConverseSneakers.mtl', function (materials) {
-        materials.preload();
-        var objLoader = new THREE.OBJLoader();
-        objLoader.setMaterials(materials);
-        objLoader.setPath('Content/assets/');
-        objLoader.load('ConverseSneakers.obj', function (object) {
-            scene.add(object);
-        });
-    });
-    /* Renderer */
+    var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+    scene.add( ambientLight );
+    var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+    camera.add( pointLight );
+    scene.add( camera );
+    // model
+    var onProgress = function ( xhr ) {
+        if ( xhr.lengthComputable ) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
+        }
+    };
+    var onError = function () { };
+    new THREE.MTLLoader()
+        .setPath( 'Content/assets/' )
+        .load( 'ConverseSneakers.mtl', function ( materials ) {
+            materials.preload();
+            new THREE.OBJLoader()
+                .setMaterials( materials )
+                .setPath( 'Content/assets/' )
+                .load( 'ConverseSneakers.obj', function ( object ) {
+                    object.position.y = - 95;
+                    scene.add( object );
+                }, onProgress, onError );
+        } );
+    //
     renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth/2, window.innerHeight/2);
-    renderer.setClearColor(new THREE.Color("hsl(0, 0%, 10%)"));
-    container.appendChild(renderer.domElement);
-    /* Controls */
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.25;
-    controls.enableZoom = false;
-    /* Events */
-    window.addEventListener('resize', onWindowResize, false);
-    window.addEventListener('keydown', onKeyboardEvent, false);
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    //
+    window.addEventListener( 'resize', onWindowResize, false );
 }
 function onWindowResize() {
-    camera.aspect = (window.innerWidth / 2) / (window.innerHeight / 2);
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
-function onKeyboardEvent(e) {
-    if (e.code === 'KeyL') {
-        lighting = !lighting;
-        if (lighting) {
-            ambient.intensity = 0.25;
-            scene.add(keyLight);
-            scene.add(fillLight);
-            scene.add(backLight);
-        } else {
-            ambient.intensity = 1.0;
-            scene.remove(keyLight);
-            scene.remove(fillLight);
-            scene.remove(backLight);
-        }
-    }
+function onDocumentMouseMove( event ) {
+    mouseX = ( event.clientX - windowHalfX ) / 2;
+    mouseY = ( event.clientY - windowHalfY ) / 2;
 }
+//
 function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
+    requestAnimationFrame( animate );
     render();
 }
 function render() {
-    renderer.render(scene, camera);
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+    camera.lookAt( scene.position );
+    renderer.render( scene, camera );
 }
