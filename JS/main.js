6ab1f1e1b45df027ef373be9b382deb16e78d152
Jaidon Rymer <jaidon.rymer@gmail.com>
2018-12-18 15:16:53 +0000

    Updated the shoe model

--- JS/main.js
+++ JS/main.js
@@ -41,12 +41,16 @@ function init() {
     container = $(".three-container")[0]
     document.body.appendChild(container);
     camera = new THREE.PerspectiveCamera(45, (window.innerWidth / 2) / (window.innerHeight / 2), 1, 2000);
-    camera.position.z = 250;
+    camera.position.z = 100;
     // scene
     scene = new THREE.Scene();
-    var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+
+    scene.background = new THREE.Color( 0xE8E8E8 );
+
+    var ambientLight = new THREE.AmbientLight(0xcccccc, 1);
     scene.add(ambientLight);
-    var pointLight = new THREE.PointLight(0xffffff, 0.8);
+
+    var pointLight = new THREE.PointLight(0xffffff, 0.4);
     camera.add(pointLight);
     scene.add(camera);
     // model
@@ -57,19 +61,19 @@ function init() {
         }
     };
     var onError = function () { };
-    new THREE.MTLLoader()
-        .setPath('Content/assets/')
-        .load('vans.mtl', function (materials) {
-            materials.preload();
+     new THREE.MTLLoader()
+         .setPath('Content/assets/')
+         .load('shoe.mtl', function (materials) {
+             materials.preload();
             new THREE.OBJLoader()
                 .setMaterials(materials)
                 .setPath('Content/assets/')
-                .load('vans.obj', function (object) {
-                    object.position.y = - 95;
+                .load('shoe.obj', function (object) {
+                    object.position.y = 0;
                     scene.add(object);
                 }, onProgress, onError);
-        });
-    //
+         });
+    
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
